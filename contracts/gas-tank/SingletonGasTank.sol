5e8d74a691ca3164e716ee2cc621e70aded9b7e1
livingrockrises <90545960+livingrockrises@users.noreply.github.com>
2022-12-20 14:34:28 +0400

    init with few references

---
+++ contracts/gas-tank/SingletonGasTank.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.12;
+
+contract SingletonGasTank {
+
+    // SCWRelayRequest struct
+
+    // SCWRelayData
+
+    // Accounting for paymasterId / dappIdentifier
+
+    // Relayer registration
+
+    // Execute / RelayCall
+}
